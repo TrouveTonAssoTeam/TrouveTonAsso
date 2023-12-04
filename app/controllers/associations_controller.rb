8bca7dd1a627d0bbd4ba4d0fb3f8ffed2871b3af
ShonaNortes <shonanortes.unterwald@gmail.com>
2023-12-04 17:40:36 +0100

    Sauvegarde des modifications avant changement de branche

--- app/controllers/associations_controller.rb
+++ app/controllers/associations_controller.rb
@@ -1,43 +1,30 @@
 class AssociationsController < ApplicationController
     before_action :authenticate_association!
+    
+    validates :name, :description, :city, :website, presence: true
   
     def show
-      @association = current_association
+      
     end
   
     def new
-      @association = Association.new
+      
     end
   
     def create
-      @association = Association.new(association_params)
-  
-      if @association.save
-        redirect_to @association, notice: 'Association was successfully created.'
-      else
-        render :new
-      end
+     
     end
   
     def edit
-      @association = current_association
+ 
     end
   
     def update
-      @association = current_association
   
-      if @association.update(association_params)
-        redirect_to @association, notice: 'Association was successfully updated.'
-      else
-        render :edit
-      end
     end
   
     def destroy
-      @association = current_association
-      @association.destroy
-  
-      redirect_to root_path, notice: 'Association was successfully destroyed.'
+      
     end
   
     private
