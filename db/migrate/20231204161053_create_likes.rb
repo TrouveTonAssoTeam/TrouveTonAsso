0f33e325eef4844fb2ffb34dd0c5659e6555e620
Miles1Morales <daniel.heart.bd@gmail.com>
2023-12-04 19:23:05 +0100

    like_change

--- db/migrate/20231204161053_create_likes.rb
+++ db/migrate/20231204161053_create_likes.rb
@@ -3,7 +3,7 @@ class CreateLikes < ActiveRecord::Migration[7.1]
     create_table :likes do |t|
       t.belongs_to :user, foreign_key: true
       t.belongs_to :association, foreign_key: true
-      belong_yo :likeable, polymorphic: true
+      t.belongs_to :likeable, polymorphic: true
       t.timestamps
     end
   end
