5475934dc372dfad34fa3344a77e075731a936a5
ShonaNortes <shonanortes.unterwald@gmail.com>
2023-12-05 15:19:01 +0100

    Beginnin of API Data, with page association done

--- app/controllers/associations_controller.rb
+++ app/controllers/associations_controller.rb
@@ -1,8 +1,14 @@
 class AssociationsController < ApplicationController
-    before_action :authenticate_association!
-  
-    def show
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def show
       @association = Association.find(params[:id])
-    end
-  
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :description, :city, :website])
+  end
+ 
+end
