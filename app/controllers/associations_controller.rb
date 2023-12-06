e2391a7471ac6339a3959bb8526947c599b997e0
LoulergueC <clement.loulergue@live.fr>
2023-12-06 13:10:45 +0100

    first connect with api

--- app/controllers/associations_controller.rb
+++ app/controllers/associations_controller.rb
@@ -1,5 +1,6 @@
 require 'net/http'
 require 'uri'
+require 'json'
 
 class AssociationsController < ApplicationController
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -10,7 +11,15 @@ class AssociationsController < ApplicationController
 
   def new
     uri = URI.parse("https://siva-integ1.cegedim.cloud/apim/api-asso/api/structure/#{params[:id]}")
-  response = Net::HTTP.get_response(uri)
+    response = Net::HTTP.get_response(uri)
+
+    data_hash = JSON.parse(response.body)
+    puts "data_hash : "
+    puts data_hash
+
+    if data_hash["erreur"] != nil
+      flash.now[:alert] = "Error : #{data_hash["erreur"]}"
+    end
   end
 
   def show
