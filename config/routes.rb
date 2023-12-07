7fe80f749f5ed5ddcf1b0d7577dcb6fade0cc400
Miles1Morales <daniel.heart.bd@gmail.com>
2023-12-07 19:52:14 +0100

    Oui

--- config/routes.rb
+++ config/routes.rb
@@ -22,8 +22,10 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  resources :associations
+  resources :like, only: :index
+  post 'like/:id', to: 'like#like', as: "like_asso"
+  post 'unlike/:id', to: 'like#unlike', as: "unlike_asso"
+  resources :organisations
 
   # Profile page and edit
   get 'profil', to: 'profil#show', as: 'profil'
@@ -33,3 +35,4 @@ Rails.application.routes.draw do
   end
   # Defines the root path route ("/")
   # root "posts#index"
+end
