958e43da3cff1b4d75327ad31a9b53e8cb3e9bc2
Sarah-Craze <sarahcraze13@gmail.com>
2023-12-07 13:08:23 +0100

    footer pages and footer links

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   #Other pages
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'mentionslegales', to: 'pages#mentionslegales'
+  get 'cookies', to: 'pages#cookies'
+  get 'cgu', to: 'pages#cgu'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
