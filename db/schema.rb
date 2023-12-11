f26f0a0aaed98520c21aebd3a85277b0931f8c04
GuillaumeYves <guillaume-las@hotmail.fr>
2023-12-11 20:07:33 +0100

    modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -130,13 +130,10 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_11_160839) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-
   add_foreign_key "cagnotte_statuses", "withdrawals"
   add_foreign_key "cagnottes", "organisations"
   add_foreign_key "likes", "organisations"
   add_foreign_key "likes", "users"
   add_foreign_key "withdrawals", "cagnotte_statuses"
   add_foreign_key "withdrawals", "organisations"
-  add_foreign_key "likes", "organisations"
-  add_foreign_key "likes", "users"
 end
