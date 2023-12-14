bc8fa1f7cd0ee8119a5b5c82bf51a93db6fd2885
LoulergueC <clement.loulergue@live.fr>
2023-12-14 13:13:13 +0100

    fix js working

--- app/javascript/navbar.js
+++ app/javascript/navbar.js
@@ -1,15 +1,17 @@
-const nav = document.getElementById("site-navbar");
+document.addEventListener("DOMContentLoaded", function () {
+    const nav = document.getElementById("site-navbar");
 
-nav.querySelector(".navbar-burger").addEventListener("click", () => {
-    nav.querySelector(".navbar-burger").classList.toggle("opened");
-    nav.querySelector(".navbar-links").classList.toggle("opened");
-    nav.querySelector(".navbar-links").classList.contains("opened")
-        ? nav.querySelector(".navbar-links").classList.add("animated")
-        : setTimeout(
-              () =>
-                  nav
-                      .querySelector(".navbar-links")
-                      .classList.remove("animated"),
-              300
-          );
+    nav.querySelector(".navbar-burger").addEventListener("click", () => {
+        nav.querySelector(".navbar-burger").classList.toggle("opened");
+        nav.querySelector(".navbar-links").classList.toggle("opened");
+        nav.querySelector(".navbar-links").classList.contains("opened")
+            ? nav.querySelector(".navbar-links").classList.add("animated")
+            : setTimeout(
+                  () =>
+                      nav
+                          .querySelector(".navbar-links")
+                          .classList.remove("animated"),
+                  300
+              );
+    });
 });
