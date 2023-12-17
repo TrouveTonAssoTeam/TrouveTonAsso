50c93647fdf5305c9c38617a1c186c64b1158444
Miles1Morales <daniel.heart.bd@gmail.com>
2023-12-17 14:44:30 +0100

    canonic add

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,7 @@ module ApplicationHelper
     def is_active?(link_path)
         current_page?(link_path) ? "active" : ""
     end
+        def canonical_url
+          request.original_url
+        end
 end
