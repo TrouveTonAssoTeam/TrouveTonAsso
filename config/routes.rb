944fd133768fbfe9f91eb254373c042e5bfad69b
ShonaNortes <shonanortes.unterwald@gmail.com>
2023-12-18 17:37:59 +0100

    add link to withdrawals on dashboard admin

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
       resources :promoteds
       resources :users
       resources :attendances
+      resources :withdrawals
 
       root to: "attendances#index"
     end
