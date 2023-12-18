a01aae0136a23a2bd0e51ecb7dc66a79dfd75a31
LoulergueC <clement.loulergue@live.fr>
2023-12-18 14:28:52 +0100

    fix tip model

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -2,5 +2,6 @@ class Tip < ApplicationRecord
     belongs_to :donation
     has_one :user, through: :donation
 
-    validates :amount, presence: true, numericality: true, default: 0
+    attribute :amount, default: 0
+    validates :amount, presence: true, numericality: true
 end
